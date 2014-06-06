a27b209cc42d1795584a4a129799e51662cfef75
jorge_filho <jorge_filho@SNSOO1EP01VM035.saojose.sc.senai.br>
2014-06-06 20:29:01 -0300

    Nutricao_pacotes

---
+++ Nutricao/src/br/senai/nutricao/dao/PacienteDao.java
@@ -0,0 +1,188 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.nutricao.dao;
+
+import br.senai.nutricao.connection.ConnectionFactory;
+import br.senai.nutricao.entity.PacienteBean;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jorge_filho
+ */
+public class PacienteDao {
+
+    private Connection connection;
+
+    public PacienteDao() {
+        this.connection = new ConnectionFactory().getConnection();
+
+    }
+
+    public void adiciona(PacienteBean pacienteBean) {
+
+        String sql = "insert into clientes "
+                + "(id, nome, cpf, rg, sexo, endereco, uf, cep, bairro, telefoneResidencial,"
+                + " telefoneAlternativo, celular, email, dataNascimento, "
+                + " login, senha, numeroCadastro, nm_responsavel)"
+                + " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        try {
+            // prepared statement para inserção
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            // seta os valores
+            stmt.setInt(1, pacienteBean.getId());
+            stmt.setString(2, pacienteBean.getNome());
+            stmt.setString(3, pacienteBean.getCpf());
+            stmt.setString(4, pacienteBean.getRg());
+            stmt.setString(5, pacienteBean.getSexo());
+            stmt.setString(6, pacienteBean.getEndereco());
+            stmt.setString(7, pacienteBean.getUf());
+            stmt.setString(8, pacienteBean.getCep());
+            stmt.setString(9, pacienteBean.getBairro());
+            stmt.setString(10, pacienteBean.getTelefoneResidencial());
+            stmt.setString(11, pacienteBean.getTelefoneAlternativo());
+            stmt.setString(12, pacienteBean.getTelefoneCelular());
+            stmt.setString(13, pacienteBean.getEmail());
+            stmt.setDate(14, (Date) pacienteBean.getDataNascimento());
+            stmt.setString(15, pacienteBean.getLogin());
+            stmt.setString(16, pacienteBean.getSenha());
+            stmt.setInt(16, pacienteBean.getNumeroCadastro());
+            stmt.setString(17, pacienteBean.getResponsavel());
+
+            // executa
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public PacienteBean getPesquisaPaciente(PacienteBean pacienteBean) {
+        try {
+            PacienteBean paciente = pacienteBean;
+            PreparedStatement stmt = this.connection.
+                    prepareStatement("select * from paciente where cpf=?");
+
+            stmt.setString(1, paciente.getCpf());
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                // criando o objeto Contato
+                PacienteBean resultPesquisa = new PacienteBean();
+                resultPesquisa.setId(rs.getInt("id"));
+                resultPesquisa.setNome(rs.getString("nome"));
+                resultPesquisa.setCpf(rs.getString("cpf"));
+                resultPesquisa.setRg(rs.getString("rg"));
+                resultPesquisa.setSexo(rs.getString("sexo"));
+                resultPesquisa.setUf(rs.getString("uf"));
+                resultPesquisa.setCep(rs.getString("cep"));
+                resultPesquisa.setBairro(rs.getString("bairro"));
+                resultPesquisa.setTelefoneResidencial(rs.getString("telefoneResidencial"));
+                resultPesquisa.setTelefoneAlternativo(rs.getString("telefoneAlternativo"));
+                resultPesquisa.setTelefoneCelular(rs.getString("telefoneCelular"));
+                resultPesquisa.setEmail(rs.getString("email"));
+                resultPesquisa.setDataNascimento(rs.getDate("dtNascimento"));
+                resultPesquisa.setNumeroCadastro(rs.getInt("numeroCadastro"));
+                resultPesquisa.setResponsavel(rs.getString("nm_responsavel"));
+            }
+
+            rs.close();
+            stmt.close();
+            return paciente;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<PacienteBean> getListaPacientes() {
+        try {
+            List<PacienteBean> listPacientes = new ArrayList<PacienteBean>();
+            PreparedStatement stmt = this.connection.
+                    prepareStatement("select * from paciente");
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                // criando o objeto Contato
+                PacienteBean paciente = new PacienteBean();
+                paciente.setId(rs.getInt("id"));
+                paciente.setNome(rs.getString("nome"));
+                paciente.setCpf(rs.getString("cpf"));
+                paciente.setRg(rs.getString("rg"));
+                paciente.setSexo(rs.getString("sexo"));
+                paciente.setUf(rs.getString("uf"));
+                paciente.setCep(rs.getString("cep"));
+                paciente.setBairro(rs.getString("bairro"));
+                paciente.setTelefoneResidencial(rs.getString("telefoneResidencial"));
+                paciente.setTelefoneAlternativo(rs.getString("telefoneAlternativo"));
+                paciente.setTelefoneCelular(rs.getString("telefoneCelular"));
+                paciente.setEmail(rs.getString("email"));
+                paciente.setDataNascimento(rs.getDate("dtNascimento"));
+                paciente.setNumeroCadastro(rs.getInt("numeroCadastro"));
+                paciente.setResponsavel(rs.getString("nm_responsavel"));
+
+                // adicionando o objeto à lista
+                listPacientes.add(paciente);
+            }
+            rs.close();
+            stmt.close();
+            return listPacientes;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void altera(PacienteBean paciente) {
+        String sql = "update contatos set nome=?, cpf=?, rg=?, sexo=?, uf=?"
+                + ", cep=?, bairro=?, telefoneResidencial=? "
+                + ", telefoneAlternativo=?, telefoneCelular=?, email=?, dtNascimento=?"
+                + ", numeroCadastro=?, nm_responsavel , where id=?";
+
+        try {
+            PreparedStatement stmt = connection
+                    .prepareStatement(sql);
+            stmt.setString(1, paciente.getNome());
+            stmt.setString(2, paciente.getCpf());
+            stmt.setString(3, paciente.getRg());
+            stmt.setString(4, paciente.getSexo());
+            stmt.setString(5, paciente.getUf());
+            stmt.setString(6, paciente.getCep());
+            stmt.setString(7, paciente.getBairro());
+            stmt.setString(8, paciente.getTelefoneResidencial());
+            stmt.setString(9, paciente.getTelefoneAlternativo());
+            stmt.setString(10, paciente.getTelefoneCelular());
+            stmt.setString(11, paciente.getEmail());
+            stmt.setDate(12, (Date) paciente.getDataNascimento());
+            stmt.setInt(13, paciente.getNumeroCadastro());
+            stmt.setString(14, paciente.getResponsavel());
+            stmt.setInt(15, paciente.getId());
+
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void remove(PacienteBean paciente) {
+        try {
+            PreparedStatement stmt = connection
+                    .prepareStatement("delete from paciente where id=?");
+            stmt.setLong(1, paciente.getId());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Nutricao/src/br/senai/nutricao/entity/PessoaBean.java
@@ -0,0 +1,161 @@
+package br.senai.nutricao.entity;
+
+import java.util.Date;
+
+public class PessoaBean {
+
+    protected int id;
+    protected String nome;
+    protected String cpf;
+    protected String sexo;
+    protected String endereco;
+    protected String uf;
+    protected String cep;
+    protected String bairro;
+    protected String telefoneResidencial;
+    protected String telefoneAlternativo;
+    protected String telefoneCelular;
+    protected String email;
+    protected Date dataNascimento;
+    protected String rg;
+    protected String login;
+    protected String senha;
+    protected Integer numeroCadastro;
+
+    public Integer getNumeroCadastro() {
+        return numeroCadastro;
+    }
+
+    public void setNumeroCadastro(Integer numeroCadastro) {
+        this.numeroCadastro = numeroCadastro;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getTelefoneResidencial() {
+        return telefoneResidencial;
+    }
+
+    public void setTelefoneResidencial(String telefoneResidencial) {
+        this.telefoneResidencial = telefoneResidencial;
+    }
+
+    public String getTelefoneAlternativo() {
+        return telefoneAlternativo;
+    }
+
+    public void setTelefoneAlternativo(String telefoneAlternativo) {
+        this.telefoneAlternativo = telefoneAlternativo;
+    }
+
+    public String getTelefoneCelular() {
+        return telefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+}
